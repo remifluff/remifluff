9ce6ffb61cf2d01eec109e42f6805091d7dec2c9
remi-freer <remi@remifreer.com>
2024-04-02 17:01:00 +1100

    yayayayay

---
+++ project_template.js
@@ -0,0 +1,19 @@
+// var html = '<div class="navlink"><a href="index.html">current</a></div>';
+
+// var section1 = ["catalogue", "howpow"]; //write one-word link names in this list
+// for (i = 0; i < section1.length; i++) {
+//   html +=
+//     '<div class="navlink"><a href="' +
+//     section1[i] +
+//     '.html">' +
+//     section1[i] +
+//     "</a></div>";
+// }
+var html = "<h1>previous collection devices:</h1>";
+html += '<link href="/style.css" rel="stylesheet" type="text/css">';
+html +=
+  '<script type="module" src="https://cdn.jsdelivr.net/gh/zerodevx/zero-md@2/dist/zero-md.min.js"></script>';
+
+document.getElementById("project_template").innerHTML = html;
+
+// html += '<a href="https://templaterr.neocities.org/"> < img src = "https://templaterr.neocities.org/templaterr1.png" ></a > '
--- projects/example/example.md
+++ projects/example/example.md
@@ -2,6 +2,10 @@
 
 ## h2 Heading
 
+<div style="padding:100% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/814108726?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" style="position:absolute;top:0;left:0;width:100%;height:100%;" title="blossom"></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+
+![Drag Racing](lil_guy.png)
+
 ### h3 Heading
 
 #### h4 Heading
